1d5fa358a6820e0061da004ea009a7e6737c150f
pranav <prane1957@gmail.com>
2023-05-01 19:09:00 +0530

    test commit

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from modules import test
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,6 @@ def contact(request):
 
 def about(request):
     return render(request,"about.html")
+
+def test_logic(request):
+    return HttpResponse(test.logic())
